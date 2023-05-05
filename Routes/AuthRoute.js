70f9f01f1745710b5fcd0abd1dd1eecf1d8c3e23
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-06 00:11:32 +0530

    Created Forgot Password API

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,12 +1,13 @@
-const { register, login, verifyUser, reSendOTP, forgotPasswordUserVerify } = require("../Controller/AuthController");
+const { register, login, verifyUser, reSendOTP, forgotPasswordUserVerify, forgotPassword } = require("../Controller/AuthController");
 
 const router = require("express").Router();
 
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/verifyUser/{userId}").post(verifyUser);
-router.route("/reSendOTP/{userId}").post(reSendOTP)
-router.route("/forgotPasswordUserVerify").post(forgotPasswordUserVerify)
+router.route("/verifyUser/:userId").post(verifyUser);
+router.route("/reSendOTP/:userId").post(reSendOTP);
+router.route("/forgotPasswordUserVerify").post(forgotPasswordUserVerify);
+router.route("/forgotPassword/:userId").post(forgotPassword);
 
 module.exports = router;
