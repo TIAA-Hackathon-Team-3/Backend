a77bce217b82b69f3ad0d8704f36b5a98a95a0d3
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-05 23:43:18 +0530

    Login API created

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -16,7 +16,7 @@ exports.register = async (req, res, next) => {
             lastName: lastName,
             email: email,
             password: password,
-            userType: USER,
+            Role: USER,
             verified: false
         });
 
@@ -33,5 +33,40 @@ exports.register = async (req, res, next) => {
         return res.status(200).json(success(`${firstName} ${lastName} is register successfully`, { id: user._id }))
     } catch (err) {
         res.status(400).json({ error: true, message: err.message });
+    }
+}
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: true, message: "Invalid Credentials. Please provide all required fields" });
+        }
+        const user = await Users.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: true, message: "User doesn't exist" });
+        }
+        if(!user.verified){
+            return res.status(200).json({ error: true, message: `${user.firstName} is not verified` });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ error: true, message: "Invalid Credentials. Ohhh ,Password is wrong" });
+        }
+        const token = getJwtToken({id: user._id, Role : user.Role});
+        const response ={
+            id:user._id,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            email:user.email,
+            Role:user.Role,
+            profilePic:user.profilePic,
+            verified: user.verified,
+            token
+        }
+        return res.status(200).json(success(`${user.firstName} Login Successful`, response))
+
+    } catch (error) {
+        res.status(400).json({ error: true, message: error.message });
     }
 }
