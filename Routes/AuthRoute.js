f1fb2beddf394344025ef8471024ffe642577853
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-05 23:58:59 +0530

    Create Resend API

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -1,4 +1,4 @@
-const { register, login, verifyUser } = require("../Controller/AuthController");
+const { register, login, verifyUser, reSendOTP } = require("../Controller/AuthController");
 
 const router = require("express").Router();
 
@@ -6,5 +6,6 @@ const router = require("express").Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/verifyUser/{userId}").post(verifyUser);
+router.route("/reSendOTP/{userId}").post(reSendOTP)
 
 module.exports = router;
