296d390ba7a83168311c283151a4dce34f6f4b02
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-05-08 13:21:25 +0530

    block and unblock user API

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -1,8 +1,38 @@
 const User = require("../Model/User");
-const {success} = require("../Helper/Response.js")
-exports.getAllUsers=async(req,res,next)=>{
+const { success } = require("../Helper/Response.js")
+
+exports.getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
+        return res.status(200).json(success("", users));
+    } catch (err) {
+        res.status(400).json({ error: true, message: err.message });
+    }
+}
+
+exports.blockUser = async (req, res, next) => {
+    try {
+        const { blockUserId } = req.params;
+        // const users = await User.find(_id:blockUserId);
+        await User.updateOne({ _id: blockUserId }, {
+            $set: {
+                blocked: true
+            }
+        })
+        return res.status(200).json(success("", users));
+    } catch (err) {
+        res.status(400).json({ error: true, message: err.message });
+    }
+}
+exports.unblockUser = async (req, res, next) => {
+    try {
+        const { blockUserId } = req.params;
+        // const users = await User.find(_id:blockUserId);
+        await User.updateOne({ _id: blockUserId }, {
+            $set: {
+                blocked: false
+            }
+        })
         return res.status(200).json(success("", users));
     } catch (err) {
         res.status(400).json({ error: true, message: err.message });
